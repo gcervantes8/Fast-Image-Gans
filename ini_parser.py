0186d62fc1d6895dcc06028bde84472f3c23745e
Jerry C <gcervantes8@miners.utep.edu>
2020-04-28 23:54:59 -0600

    Modified how ini parses (read only)

---
+++ ini_parser.py
@@ -0,0 +1,16 @@
+import configparser
+
+
+# Method to read config file taken from https://stackoverflow.com/a/29925487/10062180
+# CAN ONLY BE USED FOR READ ACCESS OF INI FILES
+class AttrDict(dict):
+
+    def __init__(self, *args, **kwargs):
+        super(AttrDict, self).__init__(*args, **kwargs)
+        self.__dict__ = self
+
+
+def read(config_file_path):
+    config = configparser.ConfigParser(dict_type=AttrDict)
+    config.read(config_file_path)
+    return config
