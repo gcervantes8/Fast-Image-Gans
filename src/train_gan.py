c2b507e20127d1621024d38bf66fc1bb4c9d3240
Gerardo Cervantes <gcervantes8@miners.utep.edu>
2020-05-21 14:07:25 -0600

    Cleaned training script, and fixed logging bug

--- src/train_gan.py
+++ src/train_gan.py
@@ -10,7 +10,7 @@ Purpose: Train the GAN (Generative Adversarial Network) model
 from __future__ import print_function
 import torch.nn as nn
 import torch.optim as optim
-import torch.utils.data
+import torch
 
 from src import ini_parser, saver_and_loader, os_helper, create_model
 
@@ -21,72 +21,61 @@ import time
 
 if __name__ == '__main__':
 
-    #Config file
+    # Config file
     config_file_path = 'model_config.ini'
-    ini_config = ini_parser.read(config_file_path)
+    config = ini_parser.read(config_file_path)
 
-    output_dir = ini_config['CONFIGS']['output_dir']
+    output_dir = config['CONFIGS']['output_dir']
     os_helper.is_valid_dir(output_dir, 'Output image directory is invalid' +
-                                        '\nPath is not a directory: ' + output_dir)
+                                       '\nPath is not a directory: ' + output_dir)
     # Each trained model will have it's own directory
     # saved with the architecture, config file, trained and generated images, trained models, and training information
     run_dir, run_id = os_helper.create_run_dir(output_dir)
 
-    #Logging
     log_path = os.path.join(run_dir, 'train.log')
     logging.basicConfig(filename=log_path, level=logging.INFO)
     # Everything logged will also be outputted to stdout (printed)
     logging.getLogger().addHandler(logging.StreamHandler())
-    logging.info('Directory ', run_dir, ' created, training output will be saved here')
+    logging.info('Directory ' + run_dir + ' created, training output will be saved here')
 
     shutil.copy(config_file_path, os.path.abspath(run_dir))
     logging.info('Copied config file!')
     saver_and_loader.save_gan_files(run_dir)
     logging.info('Copied the Generator and Discriminator files')
 
-    data_dir = ini_config['CONFIGS']['dataroot']
+    data_dir = config['CONFIGS']['dataroot']
     os_helper.is_valid_dir(data_dir, 'Invalid training data directory' +
-                                  '\nPath is an invalid directory: ' + data_dir)
+                                     '\nPath is an invalid directory: ' + data_dir)
 
-    data_loader = create_model.create_data_loader(ini_config, data_dir)
+    data_loader = create_model.create_data_loader(config, data_dir)
     saver_and_loader.save_training_images(data_loader, run_dir)
 
-    latent_vector_size = int(ini_config['CONFIGS']['latent_vector_size'])
-
-    netG, netD, device = create_model.create_gan_instances(ini_config)
+    # Create model
+    netG, netD, device = create_model.create_gan_instances(config)
+    saver_and_loader.save_architecture(netG, netD, run_dir)
     logging.info('Is GPU available? ' + str(torch.cuda.is_available()))
 
     # Apply weights_init function to randomly initialize all weights to mean=0, stdev=0.2.
     netD.apply(create_model.weights_init)
     netG.apply(create_model.weights_init)
 
-    saver_and_loader.save_architecture(netG, netD, run_dir)
-
     # Initialize BCELoss function
     criterion = nn.BCELoss()
 
-    # Create batch of latent vectors used to visualize the generator
+    # Create batch of latent vectors to visualize the generator
+    latent_vector_size = int(config['CONFIGS']['latent_vector_size'])
     fixed_noise = torch.randn(64, latent_vector_size, 1, 1, device=device)
 
     fake_label, real_label = 0, 1
 
-    beta1 = float(ini_config['CONFIGS']['beta1'])
-    lr = float(ini_config['CONFIGS']['lr'])
+    beta1 = float(config['CONFIGS']['beta1'])
+    lr = float(config['CONFIGS']['lr'])
 
     # Setup Adam optimizers for both G and D
     optimizerD = optim.Adam(netD.parameters(), lr=lr, betas=(beta1, 0.999))
     optimizerG = optim.Adam(netG.parameters(), lr=lr, betas=(beta1, 0.999))
 
-    # Training Loop
-
-    # Lists to keep track of progress
-    img_list = []
-    G_losses = []
-    D_losses = []
-    iters = 0
-
-    n_epochs = int(ini_config['CONFIGS']['num_epochs'])
-
+    n_epochs = int(config['CONFIGS']['num_epochs'])
     logging.info("Starting Training Loop...")
 
     for epoch in range(n_epochs):
@@ -99,20 +88,19 @@ if __name__ == '__main__':
             ###########################
             # Train with all-real batch
             netD.zero_grad()
-            # Format batch
+
             real_cpu = data[0].to(device)
             b_size = real_cpu.size(0)
             label = torch.full((b_size,), real_label, device=device)
-            # Forward pass real batch through D
+
             output = netD(real_cpu).view(-1)
-            # Calculate loss on all-real batch
+
             errD_real = criterion(output, label)
             # Calculate gradients for D in backward pass
             errD_real.backward()
             D_x = output.mean().item()
 
             # Train with all-fake batch
-            # Generate batch of latent vectors
             noise = torch.randn(b_size, latent_vector_size, 1, 1, device=device)
             # Generate fake image batch with G
             fake = netG(noise)
@@ -136,7 +124,6 @@ if __name__ == '__main__':
             label.fill_(real_label)  # fake labels are real for generator cost
             # Since we just updated D, perform another forward pass of all-fake batch through D
             output = netD(fake).view(-1)
-            # Calculate G's loss based on this output
             errG = criterion(output, label)
             # Calculate gradients for G
             errG.backward()
@@ -147,16 +134,10 @@ if __name__ == '__main__':
             # Output training stats
             if i % 50 == 0:
                 logging.info('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f\tTime: %.2fs'
-                      % (epoch, n_epochs, i, len(data_loader),
-                         errD.item(), errG.item(), D_x, D_G_z1, D_G_z2, time.time() - train_seq_start_time))
+                             % (epoch, n_epochs, i, len(data_loader), errD.item(), errG.item(), D_x, D_G_z1, D_G_z2,
+                                time.time() - train_seq_start_time))
                 train_seq_start_time = time.time()
-            # Save Losses for plotting later
-            G_losses.append(errG.item())
-            D_losses.append(errD.item())
 
-            iters += 1
-
-        # Check how the generator is doing by saving G's output on fixed_noise
         logging.info('Saving fake images')
         with torch.no_grad():
             fake = netG(fixed_noise).detach().cpu()
@@ -164,8 +145,9 @@ if __name__ == '__main__':
         logging.info(fake_img_output_path)
         saver_and_loader.save_images(fake, fake_img_output_path)
 
+        # Saves models
         generator_path = run_dir + '/' + 'generator_epoch_' + str(epoch) + '.pt'
         discriminator_path = run_dir + '/' + 'discriminator_epoch_' + str(epoch) + '.pt'
         saver_and_loader.save_model(netG, netD, generator_path, discriminator_path)
-logging.info('Training complete! Models and output saved in the output directory:')
-logging.info(run_dir)
+    logging.info('Training complete! Models and output saved in the output directory:')
+    logging.info(run_dir)
