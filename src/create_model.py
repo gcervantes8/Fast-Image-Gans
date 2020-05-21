bb7147b364a47d1e4a041a11d437fc9a1c780ab4
Gerardo Cervantes <gcervantes8@miners.utep.edu>
2020-05-21 14:32:48 -0600

    Added more pre-processing (transforms)

--- src/create_model.py
+++ src/create_model.py
@@ -23,6 +23,7 @@ def create_data_loader(config, data_dir):
                                           transform=transforms.Compose([
                                               transforms.Resize(image_size),
                                               transforms.CenterCrop(image_size),
+                                              transforms.ColorJitter(brightness=0.1, contrast=0.05, saturation=0.1, hue=0.05),
                                               transforms.ToTensor(),
                                               transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
                                           ]))
