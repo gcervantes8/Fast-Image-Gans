6f2d52701c9f2e4cf836019cca333bcc1a6f7961
Gerardo Cervantes <geras22222c@gmail.com>
2020-06-10 02:11:47 +0000

    Fixed bugs with multi-threading

--- src/data_scripts/prune_data.py
+++ src/data_scripts/prune_data.py
@@ -17,6 +17,8 @@ import os
 from PIL import Image
 from keras.preprocessing.image import img_to_array
 from threading import Thread
+from keras import backend as K
+import tensorflow as tf
 
 
 def open_image(path):
@@ -45,15 +47,18 @@ def crop_and_resize_image(pil_img):
 
 
 # Images with white or black screen or low prediction values will be moved from src location to dest location
-def move_uncertain_images(src_image_paths, dst_path, model):
+def move_uncertain_images(src_image_paths, dst_path, model, session, graph):
     resized_images = []
     for src_image_path in src_image_paths:
         image = open_image(src_image_path)
         image = crop_and_resize_image(image)
         resized_images.append(image)
 
-    np_images = np.array(img_to_array(resized_images)).astype(np.float32) / 255
-    nn_outputs = model.predict(np_images)
+    np_images = [img_to_array(resized_image) for resized_image in resized_images]
+    np_images = np.array(np_images).astype(np.float32) / 255
+    with session.as_default():
+        with graph.as_default():
+            nn_outputs = model.predict(np_images)
     # Can be optimized
     for i, img_prediction in enumerate(nn_outputs):
         pred = np.argmax(img_prediction)
@@ -65,8 +70,8 @@ def move_uncertain_images(src_image_paths, dst_path, model):
 
 if __name__ == "__main__":
 
-    image_directory = r'D:\mario_data\test-delete'
-    pruned_img_dir = r'D:\mario_data\pruned_test-delete'
+    image_directory = r'fazerlazer'
+    pruned_img_dir = r'fazerlazer_pruned'
     model_file_path = 'sm64Model7.hdf5'
     if not os.path.isdir(image_directory):
         raise OSError('Invalid image dir')
@@ -83,22 +88,27 @@ if __name__ == "__main__":
 
     print('Number of images: ' + str(len(img_paths)))
 
-    from keras import backend as K
+    # Load model
     K.clear_session()
     from keras.models import load_model
     nn_model = load_model(model_file_path)
-
-    batch_size = 64
+    # For multi-threading
+    session = K.get_session()
+    nn_model._make_predict_function()
+    graph = tf.get_default_graph()
+    graph.finalize()
+    batch_size = 256
     is_finished = False
     threads = []
     while not is_finished:
 
         if len(img_paths) <= batch_size:
             # call with this thread
-            move_uncertain_images(img_paths, pruned_img_dir, nn_model)
+            move_uncertain_images(img_paths, pruned_img_dir, nn_model, session, graph)
             is_finished = True
         batch_img_paths = img_paths[:batch_size]
-        thread = Thread(target=move_uncertain_images, args=(batch_img_paths,pruned_img_dir,nn_model,))
+        thread = Thread(target=move_uncertain_images, args=(batch_img_paths,pruned_img_dir,nn_model,session, graph,))
+        thread.start()
         threads.append(thread)
         img_paths = img_paths[batch_size:]
 
