afff97ce47c12dd4fd012bdb086f8669efedab6d
Gerardo Cervantes <gcervantes8@miners.utep.edu>
2020-06-23 22:04:12 +0000

    Added a trained model (128 by 96 images) output

---
+++ output/KDTI/Generator.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 11 00:23:38 2020
+
+@author: Gerardo Cervantes
+
+Purpose: The Generator class part of the GAN.  Customizable in the creation.
+The class takes in a latent vector to generate new images (in the forward pass)
+"""
+
+import torch.nn as nn
+from torch.nn.utils import spectral_norm
+
+
+class Generator(nn.Module):
+    def __init__(self, num_gpu, latent_vector_size, ngf, num_channels):
+        super(Generator, self).__init__()
+        self.n_gpu = num_gpu
+        self.main = nn.Sequential(
+            # (in-1) * s - 2 * p + (1 * (k - 1) + 1
+            # input is latent vector of given size (n_channels, 1, 1)
+            spectral_norm(nn.ConvTranspose2d(latent_vector_size, ngf * 16, kernel_size=(3, 4), stride=1, padding=0)),
+            nn.BatchNorm2d(ngf * 16),
+            nn.ReLU(True),
+            # state size: (ngf*8) x 3 x 4
+            spectral_norm(nn.ConvTranspose2d(ngf * 16, ngf * 8, kernel_size=4, stride=2, padding=1)),
+            nn.BatchNorm2d(ngf * 8),
+            nn.ReLU(True),
+            # state size: (ngf*4) x 6 x 8
+            spectral_norm(nn.ConvTranspose2d(ngf * 8, ngf * 4, kernel_size=4, stride=2, padding=1)),
+            nn.BatchNorm2d(ngf * 4),
+            nn.ReLU(True),
+            # state size: (ngf*2) x 12 x 16
+            spectral_norm(nn.ConvTranspose2d(ngf * 4, ngf * 2, kernel_size=4, stride=2, padding=1)),
+            nn.BatchNorm2d(ngf * 2),
+            nn.ReLU(True),
+            # state size: (ngf*2) x 24 x 32
+            spectral_norm(nn.ConvTranspose2d(ngf * 2, ngf, kernel_size=4, stride=2, padding=1)),
+            nn.BatchNorm2d(ngf),
+            nn.ReLU(True),
+            # state size: (ngf) x 48 x 64
+            spectral_norm(nn.ConvTranspose2d(ngf, num_channels, kernel_size=4, stride=2, padding=1)),
+            nn.Tanh()
+            # state size: (nc) x 96 x 128
+        )
+
+    def forward(self, generator_input):
+        return self.main(generator_input)
+
