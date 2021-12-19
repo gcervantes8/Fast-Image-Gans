b859ff0c645d96838e21f01b6f62a89b2614b232
gerry <geras22222c@gmail.com>
2021-12-19 10:50:56 -0700

    Added a more helpful warning to help when the dataroot directory given is wrong

--- src/create_model.py
+++ src/create_model.py
@@ -20,15 +20,19 @@ def create_data_loader(config, data_dir):
 
     image_height = int(config['CONFIGS']['image_height'])
     image_width = int(config['CONFIGS']['image_width'])
-    data_set = torch_data_set.ImageFolder(root=data_dir,
-                                          transform=transforms.Compose([
-                                              transforms.Resize((image_height+2, image_width+2)),
-                                              transforms.RandomCrop((image_height, image_width)),
-                                              transforms.ColorJitter(brightness=0.1, contrast=0.05,
-                                                                     saturation=0.1, hue=0.05),
-                                              transforms.ToTensor(),
-                                              transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-                                          ]))
+    try:
+        data_set = torch_data_set.ImageFolder(root=data_dir,
+                                              transform=transforms.Compose([
+                                                  transforms.Resize((image_height+2, image_width+2)),
+                                                  transforms.RandomCrop((image_height, image_width)),
+                                                  transforms.ColorJitter(brightness=0.1, contrast=0.05,
+                                                                         saturation=0.1, hue=0.05),
+                                                  transforms.ToTensor(),
+                                                  transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                                              ]))
+    except FileNotFoundError:
+        raise FileNotFoundError('Data directory provided should contain directories that have images in them, '
+                                'directory provided: ' + data_dir)
 
     batch_size = int(config['CONFIGS']['batch_size'])
     n_workers = int(config['CONFIGS']['workers'])
