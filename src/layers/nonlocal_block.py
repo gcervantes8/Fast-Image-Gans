96dd97a2dcb06a9554010d167980606f5536732d
gcervantes8 <gercervantes7@gmail.com>
2022-08-10 10:33:03 -0400

    Made the Self Attention layer more similar to what is done in the SAGAN model, instead of the original paper that mentions the layer

--- src/layers/nonlocal_block.py
+++ src/layers/nonlocal_block.py
@@ -21,11 +21,14 @@ class NonLocalBlock(nn.Module):
         self.block_channels_2 = block_channels_2
         self.conv_delta = nn.Conv2d(channels, block_channels_1, kernel_size=1, padding='same')
         self.conv_phi = nn.Conv2d(channels, block_channels_1, kernel_size=1, padding='same')
+        self.pooling_phi = nn.MaxPool2d(kernel_size=2)
         self.conv_g = nn.Conv2d(channels, block_channels_2, kernel_size=1, padding='same')
-        self.softmax = nn.Softmax(dim=1)  # Should not be dim 0, since that's batch
+        self.pooling_g = nn.MaxPool2d(kernel_size=2)
+        self.softmax = nn.Softmax(dim=-1)  # Should not be dim 0, since that's batch
         self.conv_last = nn.Conv2d(block_channels_2, channels, kernel_size=1, padding='same')
+        self.gamma = nn.Parameter(torch.zeros(1))
 
-    # res_input is of size (B, C, H, W)
+        # res_input is of size (B, C, H, W)
     def forward(self, nonlocal_input):
         batch_size = nonlocal_input.size(0)
         height = nonlocal_input.size(2)
@@ -40,32 +43,42 @@ class NonLocalBlock(nn.Module):
 
         # conv phi out is [B, block_channels, H, W]
         phi_out = self.conv_phi(nonlocal_input)
-        # [block_channels_1, B, H, W]
+        # [B, block_channels, H/2, W/2]
+        phi_out = self.pooling_phi(phi_out)
+
+        # [block_channels_1, B, H/2, W/2]
         phi_out = torch.permute(phi_out, (1, 0, 2, 3))
-        # [B, block_channels_1, H*W]
+
+        # [B, block_channels_1, H*W/4]
         phi_out = torch.reshape(phi_out, [batch_size, self.block_channels_1, -1])
 
-        # [B, H*W, H*W]
-        first_mult_out = torch.bmm(delta_out, phi_out)
+        # [B, H*W, H*W/4]
+        mult_out = torch.bmm(delta_out, phi_out)
 
-        first_mult_out = torch.reshape(first_mult_out, [batch_size, -1])
+        # [B, H*W, H*W/4]
+        mult_out = self.softmax(mult_out)
 
-        softmax_mult = self.softmax(first_mult_out)
-        softmax_mult = torch.reshape(softmax_mult, [batch_size, height * width, height * width])
+        # mult_out = torch.reshape(mult_out, [batch_size, -1])
+
+        # mult_out = torch.reshape(mult_out, [batch_size, height * width, height * width])
 
         # conv g out is [B, block_channels_2, H, W]
         g_out = self.conv_g(nonlocal_input)
 
-        # [B, H, W, block_channels_2]
+        # [B, block_channels_2, H/2, W/2]
+        g_out = self.pooling_phi(g_out)
+
+        # [B, H/2, W/2, block_channels_2]
         g_out = torch.permute(g_out, (0, 2, 3, 1))
 
-        # [B, H*W, block_channels_2]
+        # [B, H*W/4, block_channels_2]
         g_out = torch.reshape(g_out, [batch_size, -1, self.block_channels_2])
 
-        mult_out = torch.bmm(softmax_mult, g_out)
+        # [B, H*W, block_channels_2]
+        mult_out = torch.bmm(mult_out, g_out)
         mult_out = torch.reshape(mult_out, [batch_size, height, width, self.block_channels_2])
         # [B, C, H, W]
         mult_out = torch.permute(mult_out, (0, 3, 1, 2))
 
         # [B, C, H, W]
-        return torch.add(nonlocal_input, self.conv_last(mult_out))
+        return torch.add(self.gamma * self.conv_last(mult_out), nonlocal_input)
