Fixed a bug that was introduced by gradient accumulation.  Gradient accumulation feature is temporarily disabled.  Also added partial implementation of training the discriminator 2 steps per 1 gan step.

src/gan_model.py
@@ -11,6 +11,7 @@ import torch
 import torch.optim as optim
 from src.losses.loss_functions import supported_loss_functions
 from src.losses.loss_functions import supported_losses
+from torch_ema import ExponentialMovingAverage
 import os
 
 
@@ -30,7 +31,8 @@ class GanModel:
         self.grad_scaler = torch.cuda.amp.GradScaler(enabled=self.mixed_precision)
         self.batch_iterations = 0
 
-        self.criterion, self.fake_label, self.real_label = supported_loss_functions(train_config['loss_function'], device=device)
+        self.criterion, self.fake_label, self.real_label = supported_loss_functions(train_config['loss_function'],
+                                                                                    device=device)
         if self.criterion is None:
             raise ValueError("Loss values options: " + str(supported_losses()))
 
@@ -38,64 +40,56 @@ class GanModel:
         self.optimizerD = optim.Adam(discriminator.parameters(), lr=discriminator_lr, betas=(beta1, beta2))
         self.optimizerG = optim.Adam(generator.parameters(), lr=generator_lr, betas=(beta1, beta2))
 
-    def update_minimax(self, real_data):
-        batch_size = real_data.size(0)
+        # Set EMA
+        ema_enabled = model_arch_config.getboolean('generator_ema')
+        ema_decay = model_arch_config['ema_decay']
+        self.ema = ExponentialMovingAverage(generator.parameters(), decay=float(ema_decay)) if ema_enabled else None
 
-        # Train discriminator with all-real batch
-        with torch.autocast(device_type=str(self.device), dtype=torch.float16, enabled=self.mixed_precision):
-            real_label_tensor = torch.full((batch_size,), self.real_label, device=self.device)
+    def update_minimax(self, real_data, train_generator=True):
+        b_size = real_data.size(0)
+        device_type, dtype = ('cpu', torch.bfloat16) if self.device.type == 'cpu' else ('cuda', torch.float16)
+
+        self.netD.zero_grad()
+        with torch.autocast(device_type=device_type, dtype=dtype, enabled=self.mixed_precision):
+            real_label = torch.full((b_size,), self.real_label, dtype=real_data.dtype, device=self.device)
             discrim_output = self.netD(real_data)
-            discrim_on_real_error = self.criterion(discrim_output, real_label_tensor)
-            discrim_on_real_error = discrim_on_real_error / self.accumulation_iterations
+            discrim_on_real_error = self.criterion(discrim_output, real_label)
 
-        # Calculate gradients for D in backward pass
         self.grad_scaler.scale(discrim_on_real_error).backward()
-        D_x = discrim_output.mean().item()
 
-        # Train discriminator with a generated batch
-        with torch.autocast(device_type=str(self.device), dtype=torch.float16, enabled=self.mixed_precision):
-            # Generate fake image batch with G, training with a fake batch
-            fake = self.netG(torch.randn(batch_size, self.latent_vector_size, 1, 1, device=self.device))
+        with torch.autocast(device_type=device_type, dtype=dtype, enabled=self.mixed_precision):
+            # Train with all-fake batch
+            noise = torch.randn(b_size, self.latent_vector_size, 1, 1, device=self.device)
+            # Generate fake image batch with G
+            fake = self.netG(noise)
+            fake_label = torch.full((b_size,), self.fake_label, dtype=real_data.dtype, device=self.device)
+            # Classify all fake batch with D
             fake_output = self.netD(fake.detach())
-
             # Calculate D's loss on the all-fake batch
-            discrim_on_fake_error = self.criterion(fake_output, torch.full((batch_size,), self.fake_label, device=self.device).reshape_as(fake_output))
-            discrim_on_fake_error = discrim_on_fake_error / self.accumulation_iterations
+            discrim_on_fake_error = self.criterion(fake_output, fake_label.reshape_as(fake_output))
 
-        # Calculate the gradients for this batch
         self.grad_scaler.scale(discrim_on_fake_error).backward()
-        D_G_z1 = fake_output.mean().item()
-
-        # ------ Generator ------
-        # Since we just updated D, perform another forward pass of all-fake batch through D
-        with torch.autocast(device_type=str(self.device), dtype=torch.float16, enabled=self.mixed_precision):
-            output_update = self.netD(fake)
-            generator_error = self.criterion(output_update, real_label_tensor)
-            generator_error = generator_error / self.accumulation_iterations
-
-        # Calculate gradients for G
-        self.grad_scaler.scale(generator_error).backward()
-        D_G_z2 = output_update.mean().item()
-
-        self.batch_iterations += 1
-        if self.batch_iterations % self.accumulation_iterations == 0:
-            # Update generator and discriminator
-            self.grad_scaler.step(self.optimizerG)
-            self.grad_scaler.step(self.optimizerD)
-            self.grad_scaler.update()
-            # Reset the grad back to 0 after a step
-            self.netG.zero_grad()
-            self.netD.zero_grad()
-            self.batch_iterations = 0
-
-        # Add the gradients from the all-real and all-fake batches
         total_discrim_error = discrim_on_real_error + discrim_on_fake_error
+        self.grad_scaler.step(self.optimizerD)
 
-        return total_discrim_error, generator_error, D_x, D_G_z1, D_G_z2
+        with torch.autocast(device_type=device_type, dtype=dtype, enabled=self.mixed_precision):
+            self.netG.zero_grad()
+            output_update = self.netD(fake)
+            generator_error = self.criterion(output_update, real_label)
+        self.grad_scaler.scale(generator_error).backward()
+        self.grad_scaler.step(self.optimizerG)
+        self.grad_scaler.update()
+        if self.ema:
+            self.ema.update()
+        return total_discrim_error, generator_error
 
     def generate_images(self, noise):
         with torch.no_grad():
-            fake = self.netG(noise).detach().cpu()
+            if self.ema:
+                with self.ema.average_parameters():
+                    fake = self.netG(noise).detach().cpu()
+            else:
+                fake = self.netG(noise).detach().cpu()
         return fake
 
     def save(self, model_dir, step_or_epoch_num):

src/train_gan.py
@@ -142,27 +142,53 @@ def train(config_file_path: str):
     profiler.start()
     n_steps = 0
     steps_in_epoch = len(data_loader)
+    train_generator = True
+    alternate_generator_training = train_config.getboolean('two_d_steps_per_g')
+    total_g_error, total_d_error = torch.zeros([1], device=device), torch.zeros([1], device=device)
+    g_steps, d_steps = 0, 0
     for epoch in range(n_epochs):
         epoch_after_loading = loaded_epoch_num + epoch
         train_seq_start_time = time.time()
         # For each batch in the data-loader
         data_time, model_time = 0, 0
         data_start_time = time.time()
-        for i, batch in enumerate(data_loader, 0):
-            n_steps += 1
-            real_data = batch[0].to(device)
-            # Normalized
-            real_data = normalize(color_transform(real_data))
-            data_time += time.time() - data_start_time
 
+        for i, batch in enumerate(data_loader, 0):
+
+            n_steps += 1
+            real_data = batch[0]
+
+            # Normalization can't be done on bloat16 operators
+            if running_on_cpu:
+                real_data = normalize(color_transform(real_data))
+            if train_config.getboolean('mixed_precision'):
+                real_data = real_data.to(torch.bfloat16) if running_on_cpu else real_data.to(torch.float16)
+
+            real_data = real_data.to(device)  # Moving to GPU is a slow operation
+            if not running_on_cpu:
+                real_data = normalize(color_transform(real_data))
+
+            data_time += time.time() - data_start_time
             model_start_time = time.time()
-            errD, errG, D_x, D_G_z1, D_G_z2 = gan_model.update_minimax(real_data)
+
+            err_discriminator, err_generator = gan_model.update_minimax(real_data, train_generator)
+            alternate_generator_training = not alternate_generator_training
+
+            if err_generator:
+                total_g_error += err_generator
+                g_steps += 1
+
+            if err_discriminator:
+                total_d_error += err_discriminator
+                d_steps += 1
+
             model_time += time.time() - model_start_time
             # Output training stats
             if n_steps % log_steps == 0:
-                logging.info('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f\tTime: %.2fs'
-                             % (epoch, n_epochs, n_steps % steps_in_epoch, steps_in_epoch, errD.item(), errG.item(),
-                                D_x, D_G_z1, D_G_z2, time.time() - train_seq_start_time))
+                # TODO add case when diving by 0 steps
+                logging.info('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tTime: %.2fs'
+                             % (epoch, n_epochs, n_steps % steps_in_epoch, steps_in_epoch, (total_d_error/d_steps).item(),
+                                (total_g_error/g_steps).item(), time.time() - train_seq_start_time))
                 logging.info(
                     'Data retrieve time: %.2fs Model updating time: %.2fs' % (data_time, model_time))
 