affb03efa9cbdad1dd75a8bc9ae9b2dcc2093e26
gcervantes8 <geras22222c@gmail.com>
2022-09-15 21:11:54 -0400

    Fixed discriminator channels in deep-biggan

--- src/discriminators/deep_res_down.py
+++ src/discriminators/deep_res_down.py
@@ -17,22 +17,27 @@ class DeepResDown(nn.Module):
         self.pooling = pooling
 
         self.relu_b1 = nn.ReLU()
-        self.conv_b2 = spectral_norm(nn.Conv2d(in_channels, int(in_channels/4), kernel_size=1, padding='same'), eps=1e-04)
+        self.conv_b2 = spectral_norm(nn.Conv2d(in_channels, int(out_channels/4), kernel_size=1, padding='same'),
+                                     eps=1e-04)
         nn.init.orthogonal_(self.conv_b2.weight)
         self.relu_b3 = nn.ReLU()
-        self.conv_b4 = spectral_norm(nn.Conv2d(int(in_channels/4), int(in_channels/4), kernel_size=3, padding='same'), eps=1e-04)
+        self.conv_b4 = spectral_norm(nn.Conv2d(int(out_channels/4), int(out_channels/4), kernel_size=3, padding='same'),
+                                     eps=1e-04)
         nn.init.orthogonal_(self.conv_b4.weight)
         self.relu_b5 = nn.ReLU()
-        self.conv_b6 = spectral_norm(nn.Conv2d(int(in_channels/4), int(in_channels/4), kernel_size=3, padding='same'), eps=1e-04)
+        self.conv_b6 = spectral_norm(nn.Conv2d(int(out_channels/4), int(out_channels/4), kernel_size=3, padding='same'),
+                                     eps=1e-04)
         nn.init.orthogonal_(self.conv_b6.weight)
         self.relu_b7 = nn.ReLU()
-        self.conv_b9 = spectral_norm(nn.Conv2d(int(in_channels/4), out_channels, kernel_size=1, padding='same'), eps=1e-04)
+        self.conv_b9 = spectral_norm(nn.Conv2d(int(out_channels/4), out_channels, kernel_size=1, padding='same'),
+                                     eps=1e-04)
         nn.init.orthogonal_(self.conv_b9.weight)
         if pooling:
             self.avg_pooling_a1 = nn.AvgPool2d(kernel_size=2)
             self.avg_pooling_b8 = nn.AvgPool2d(kernel_size=2)
         if not self.same_dim:
-            self.conv_a2 = spectral_norm(nn.Conv2d(in_channels, out_channels-in_channels, kernel_size=1, padding='same'), eps=1e-04)
+            self.conv_a2 = spectral_norm(nn.Conv2d(in_channels, out_channels-in_channels, kernel_size=1, padding='same'),
+                                         eps=1e-04)
             nn.init.orthogonal_(self.conv_a2.weight)
 
     # res_input is of size (B, C, H, W)
