cd3f07905058c359176f205597061e1b76ff900d
gcervantes8 <gercervantes7@gmail.com>
2023-07-29 19:41:02 -0400

    Modified attention layer, paper had incorrect implementation

--- src/layers/nonlocal_block.py
+++ src/layers/nonlocal_block.py
@@ -2,8 +2,8 @@ import torch
 import torch.nn as nn
 from torch.nn.utils import spectral_norm
 
-# Attention block based on https://arxiv.org/pdf/1805.08318.pdf
-# And the original paper https://arxiv.org/pdf/1711.07971.pdf
+# Decription in paper is incorrect (https://github.com/ajbrock/BigGAN-PyTorch/issues/17), corrected based on BigGAN Pytorch by ajbrock
+# Previously implemented based on https://arxiv.org/pdf/1805.08318.pdf and original paper https://arxiv.org/pdf/1711.07971.pdf
 
 
 class NonLocalBlock(nn.Module):
@@ -22,16 +22,16 @@ class NonLocalBlock(nn.Module):
         self.channels = channels
         self.block_channels_1 = block_channels_1
         self.block_channels_2 = block_channels_2
-        self.conv_delta = spectral_norm(nn.Conv2d(channels, block_channels_1, kernel_size=1, padding='same'), eps=1e-04)
+        self.conv_delta = spectral_norm(nn.Conv2d(channels, block_channels_1, kernel_size=1, padding='same', bias=False), eps=1e-04)
         nn.init.orthogonal_(self.conv_delta.weight)
-        self.conv_phi = spectral_norm(nn.Conv2d(channels, block_channels_1, kernel_size=1, padding='same'), eps=1e-04)
+        self.conv_phi = spectral_norm(nn.Conv2d(channels, block_channels_1, kernel_size=1, padding='same', bias=False), eps=1e-04)
         nn.init.orthogonal_(self.conv_phi.weight)
         self.pooling_phi = nn.MaxPool2d(kernel_size=2)
-        self.conv_g = spectral_norm(nn.Conv2d(channels, block_channels_2, kernel_size=1, padding='same'), eps=1e-04)
+        self.conv_g = spectral_norm(nn.Conv2d(channels, block_channels_2, kernel_size=1, padding='same', bias=False), eps=1e-04)
         nn.init.orthogonal_(self.conv_g.weight)
         self.pooling_g = nn.MaxPool2d(kernel_size=2)
         self.softmax = nn.Softmax(dim=-1)  # Should not be dim 0, since that's batch
-        self.conv_last = spectral_norm(nn.Conv2d(block_channels_2, channels, kernel_size=1, padding='same'), eps=1e-04)
+        self.conv_last = spectral_norm(nn.Conv2d(block_channels_2, channels, kernel_size=1, padding='same', bias=False), eps=1e-04)
         nn.init.orthogonal_(self.conv_last.weight)
         self.gamma = nn.Parameter(torch.zeros(1))
 
@@ -43,45 +43,35 @@ class NonLocalBlock(nn.Module):
 
         # conv delta out is [B, block_channels_1, H, W]
         delta_out = self.conv_delta(nonlocal_input)
-        # [B, H, W, block_channels_1]
-        delta_out = torch.permute(delta_out, (0, 2, 3, 1))
+        # [B, block_channels_1, H*W]
+        delta_out = delta_out.view(batch_size, self.block_channels_1, height*width)
         # [B, H*W, block_channels_1]
-        delta_out = torch.reshape(delta_out, [batch_size, -1, self.block_channels_1])
+        delta_out = torch.transpose(delta_out, 1, 2)
 
         # conv phi out is [B, block_channels_1, H, W]
         phi_out = self.conv_phi(nonlocal_input)
         # [B, block_channels_1, H/2, W/2]
         phi_out = self.pooling_phi(phi_out)
-
-        # [block_channels_1, B, H/2, W/2]
-        phi_out = torch.permute(phi_out, (1, 0, 2, 3))
-
         # [B, block_channels_1, H*W/4]
-        phi_out = torch.reshape(phi_out, [batch_size, self.block_channels_1, -1])
-
+        phi_out = phi_out.view(batch_size, self.block_channels_1, -1)
+       
         # [B, H*W, H*W/4]
-        mult_out = torch.bmm(delta_out, phi_out)
-
-        # [B, H*W, H*W/4]
-        mult_out = self.softmax(mult_out)
+        mult_out = self.softmax(torch.bmm(delta_out, phi_out))
+        # [B, H*W/4, H*W]
+        mult_out = torch.transpose(mult_out, 1, 2)
 
         # conv g out is [B, block_channels_2, H, W]
         g_out = self.conv_g(nonlocal_input)
-
         # [B, block_channels_2, H/2, W/2]
         g_out = self.pooling_g(g_out)
+        # [B, block_channels_2, H*W/4]
+        g_out = g_out.view(batch_size, self.block_channels_2, -1)
 
-        # [B, H/2, W/2, block_channels_2]
-        g_out = torch.permute(g_out, (0, 2, 3, 1))
 
-        # [B, H*W/4, block_channels_2]
-        g_out = torch.reshape(g_out, [batch_size, -1, self.block_channels_2])
-
-        # [B, H*W, block_channels_2]
-        mult_out = torch.bmm(mult_out, g_out)
-        mult_out = torch.reshape(mult_out, [batch_size, height, width, self.block_channels_2])
+        # [B, block_channels_2, H*W]
+        mult_out = torch.bmm(g_out, mult_out)
         # [B, block_channels_2, H, W]
-        mult_out = torch.permute(mult_out, (0, 3, 1, 2))
+        mult_out = mult_out.view(batch_size, self.block_channels_2, height, width)
 
         # [B, C, H, W]
         return torch.add(self.gamma * self.conv_last(mult_out), nonlocal_input)
